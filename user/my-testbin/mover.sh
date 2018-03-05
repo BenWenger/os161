204336b5d9c64d81772775d33c6b397af85c4248
Ben Wenger <ben.wenger@comcast.net>
2018-03-05 13:01:42 -0800

    fixing mover.sh

--- user/my-testbin/mover.sh
+++ user/my-testbin/mover.sh
@@ -1,3 +1,3 @@
 cp sysfork/build/sysfork ~/os161/root/my-testbin/sysfork
-cp sysfork/build/fork ~/os161/root/my-testbin/fork
-cp sysfork/build/getpid ~/os161/root/my-testbin/getpid
+cp fork/build/fork ~/os161/root/my-testbin/fork
+cp getpid/build/getpid ~/os161/root/my-testbin/getpid
